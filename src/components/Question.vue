656c71578d518092f42d002b49dac21b6870ff04
shnr <nara@shnr.net>
2019-08-21 19:15:01 +0900

    fa

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -2,17 +2,18 @@
   <div class="hello">
     <template v-if="!ifFinish">
       <p class="q-title">もんだい {{currentQuestion+1}}</p>
-      <span v-for="(n, i2) in values[currentQuestion].prob">
+      <span v-for="(n, i2) in values[currentQuestion].prob" class="s-num">
         {{ n }}
-        <span v-if="i2 !== (values[currentQuestion].prob.length - 1)">+</span>        
+        <span v-if="i2 !== (values[currentQuestion].prob.length - 1)" class="s-num">+</span>        
       </span>
       = 
-      <input type="text" v-model="values[currentQuestion].answer">
-      <button class="btn-next-pProblem" v-on:click="goNext()">次の問題へ</button>
+      <input type="text" v-model="values[currentQuestion].answer" class="s-ans">
+      <button class="btn-next-pProblem" v-on:click="goNext()">つぎへ</button>
     </template>
 
     <template v-if="ifFinish">
-      <div v-for="(item, i) in values">
+      <p class="q-title">けっか</p>
+      <div v-for="(item, i) in values" class="result-box" v-bind:class="{'is-box-true': item.result, 'is-box-false': !item.result}">
         <span v-for="(n, i2) in item.prob">
           {{ n }}
           <span v-if="i2 !== (item.prob.length - 1)">+</span>        
@@ -24,7 +25,7 @@
           <template v-else>X</template>
         </span>
       </div>
-      {{correctQuestionNum}}こ　せいかいだよ！
+      <p class="result-text">せいかいは、{{correctQuestionNum}}こ！</p>      
       <p><button class="btn-reload-pProblem" v-on:click="reload()">もう一度！</button></p>
     </template>
 
@@ -103,7 +104,7 @@ export default {
       if((self.currentQuestion + 1) >= self.numOfquestions){
           self.submit()
       } else {
-          if(typeof self.values[self.currentQuestion].answer === 'undefined' || self.values[self.currentQuestion].answer === "") {
+          if(typeof self.values[self.currentQuestion].answer === 'undefined' || self.values[self.currentQuestion].answer === "" || self.values[self.currentQuestion].answer === null) {
               alert("もんだいにこたえてね！")
           } else {
             self.currentQuestion++
@@ -131,5 +132,81 @@ li {
 }
 a {
   color: #42b983;
+}
+.q-title{
+  font-size: 16px;
+  line-height: 1.6;
+  color: #000;
+  margin-bottom: 4em;
+  position: relative;
+}
+.q-title:after{
+  display: block;
+  content: "";
+  width: 2em;
+  height: 2px;
+  background-color: #ddd;
+  margin: .2em auto;
+}
+.s-num{
+  font-size: 24px;
+  line-height: 1.6;
+  color: #000;
+
+}
+.s-ans{
+  font-size: 24px;
+  line-height: 1.6;
+  color: #000;
+  width: 4em;
+  border: 1px solid #999;
+  text-align: center;
+  padding: 0.4em;
+}
+.btn-next-pProblem{
+  font-size: 16px;
+  line-height: 1.6;
+  color: #000;
+  display: block;
+  margin: 2em auto;
+  border: 1px solid #999;
+  background-color: #f1f1f1;
+}
+.btn-reload-pProblem{
+  font-size: 16px;
+  line-height: 1.6;
+  color: #000;
+  display: block;
+  margin: 2em auto;
+  border: 1px solid #999;
+  background-color: #f1f1f1;
+
+}
+.result-text{
+  font-size: 16px;
+  line-height: 1.6;
+  color: #000;
+  display: block;
+  text-align: center;
+  margin-top: 2em;
+}
+.result-box{
+  width: 53.3vmin;
+  text-align: left;
+  margin: 0 auto 0;
+}
+.is-false{
+  font-size: 16px;
+  line-height: 1.6;  
+}
+.is-true{
+  font-size: 16px;
+  
+}
+.is-box-true{
+  color: #2196f3;
+}
+.is-box-false{
+  color: #999;
 }
 </style>
